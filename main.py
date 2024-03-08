ed0611d708d2aa260d7ccea76a2a12a1dc83a74f
mtraihan <r.r.kaskandar@vu.nl>
2024-03-08 21:33:45 +0100

    init base

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import tkinter  
+import customtkinter
+from pytube import YouTube     
+
 def main():
-    print("Hello World!")
-    print("This is a test for the git repository")
+
+    # System Settings
+    customtkinter.set_appearance_mode("Light")
+    customtkinter.set_default_color_theme("blue")
+
+    # Create the main window
+    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    app.geometry("1280x720")
+    app.title("PyTubeSaver") 
+
+    # Adding UI elements
+    title = customtkinter.CTkLabel(app, text="Insert a YouTube link!", font=("Arial", 20))
+    title.pack(padx=20, pady=10)
+
+    # Link input
+    url_var = tkinter.StringVar()
+    link = customtkinter.CTkEntry(app, width=700, height=40, textvariable=url_var)
+    link.pack(padx=10, pady=10)
+
+    # # Download button
+    # download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+
+    app.mainloop()
+
+
 
 
 if __name__ == '__main__':
